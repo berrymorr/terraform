24fd09491aae4f2eee125751857d865a3291a0e1
Pavel Tarasov <berrymorr@gmail.com>
2022-07-27 08:17:21 -0400

    +dynamic disks

--- machines.tf
+++ machines.tf
@@ -1,8 +1,10 @@
 
 locals {
-  virtual_machines = {
-    "server-1" = { system_cores = 1, system_memory = 1024, ip="192.168.122.21", disk=20*1024*1024*1024 },
-    "server-2" = { system_cores = 1, system_memory = 1024, ip="192.168.122.22", disk=10*1024*1024*1024 },
-    "server-zalupa" = { system_cores = 2, system_memory = 10240, ip="192.168.122.122", disk=30*1024*1024*1024 },
-  }
+  pool = "default"
+  network = { name="default", bridge="virbr0" }
+  vms = [
+    { name="server-1", cores=4, mem=4096, ip="192.168.122.21", disks=[10] },
+    { name="server-2", cores=4, mem=4096, ip="192.168.122.22", disks=[10,20] },
+    { name="server-3", cores=4, mem=4096, ip="192.168.122.23", disks=[10,20] },
+  ]
 }
--- main.tf
+++ main.tf
@@ -11,45 +11,58 @@ provider "libvirt" {
 }
 
 
+locals {
+  disks_flat = merge([for vm in local.vms : {for index, disk_size in vm.disks : ("${vm.name}_${index}") => disk_size}]...)
+  disks_dynamic = { for vm in local.vms : (vm.name) => [ for index,disk in vm.disks : "${vm.name}_${index}" ] }
+}
 
 
 resource "libvirt_volume" "os_image" {
   name   = "os_image"
-  source = "/root/govno/CentOS-7-x86_64-GenericCloud.qcow2"
+  #source = "/root/CentOS-7-x86_64-GenericCloud.qcow2"
+  source = "/root/almacloud8.qcow2"
+  pool = local.pool
 }
+
 
 resource "libvirt_cloudinit_disk" "commoninit" {
-  for_each = local.virtual_machines
-  name      = "${each.key}.iso"
-  pool      = "default" # List storage pools using virsh pool-list
-  user_data = templatefile("${path.module}/cloud_init.tftpl", {fqdn = each.key, ip = each.value.ip})
+  for_each = { for vm in local.vms : (vm.name) => vm }
+  name      = "${each.value.name}.iso"
+  pool      = local.pool
+  user_data = templatefile("${path.module}/cloud_init.tftpl", {fqdn = each.value.name, ip = each.value.ip})
 }
+
 
 resource "libvirt_volume" "volume" {
-  for_each       = local.virtual_machines
+  for_each       = local.disks_flat
   name           = "${each.key}.qcow2"
-  base_volume_id = libvirt_volume.os_image.id
-  size           = each.value.disk
+  base_volume_id = regex("[0-9]+$",each.key) == "0" ? libvirt_volume.os_image.id : null
+  size           = each.value*1024*1024*1024
+  pool           = local.pool
 }
 
-resource "libvirt_domain" "domain" {
-  for_each = local.virtual_machines
-  name     = each.key
-  memory   = each.value.system_memory
-  vcpu     = each.value.system_cores
 
-  disk {
-    volume_id = libvirt_volume.volume[each.key].id
+resource "libvirt_domain" "domain" {
+  for_each = { for vm in local.vms : (vm.name) => vm }
+  name     = each.value.name
+  memory   = each.value.mem
+  vcpu     = each.value.cores
+
+  dynamic "disk" {
+    for_each = local.disks_dynamic[each.value.name]
+    content {
+      volume_id = libvirt_volume.volume[disk.value].id
+    }
   }
 
   network_interface {
-    network_name   = "default" # List networks with virsh net-list
-    bridge         = "virbr0"
+    network_name   = local.network.name
+    bridge         = local.network.bridge
     addresses      = [each.value.ip]
-    wait_for_lease = true
+    wait_for_lease = false
   }
 
-  cloudinit = libvirt_cloudinit_disk.commoninit[each.key].id
+  cloudinit = libvirt_cloudinit_disk.commoninit["${each.value.name}"].id
 
 }
 
